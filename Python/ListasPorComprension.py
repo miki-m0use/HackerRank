1c32a58b02cc3e59f05f1e373064e21ca83c236f
pc-miki <miguelpereira3005@gmail.com>
2025-07-21 01:28:55 -0400

    resuelto

--- Python/ListasPorComprension.py
+++ Python/ListasPorComprension.py
@@ -1,14 +1,15 @@
-x = int(input())
-y = int(input())
-z = int(input())
-n = int(input())
+# x = int(input("Enter value for x: "))
+# y = int(input("Enter value for y: "))
+# z = int(input("Enter value for z: "))
+# n = int(input("Enter value for n: "))
+
+x = 2
+y = 2
+z = 2
+n = 2
 
 
-x += 1
-y += 1
-z += 1
+resp = [[i, j, z1] for i in range(x+1) for j in range(y+1)
+        for z1 in range(z+1) if i + j + z1 != n]
 
-
-respuesta = [[i, j, z1] for i in x for j in y for z1 in z if i + j + z1 != n]
-
-print(respuesta)
+print(resp)
