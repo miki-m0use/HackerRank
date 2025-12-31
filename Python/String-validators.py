5d679058fb1ec5806f6188cdaa98272183dbec45
miki-maus <miguelpereira3005@gmail.com>
2025-12-31 11:10:57 -0300

    String validators

---
+++ Python/String-validators.py
@@ -0,0 +1,41 @@
+def main(s):
+
+    for i in range(len(s)):
+        if s[i].isalnum():
+            print(True)
+            break
+    else:
+        print(False)
+
+    for i in range(len(s)):
+        if s[i].isalpha():
+            print(True)
+            break
+    else:
+        print(False)    
+    
+    for i in range(len(s)):
+        if s[i].isdigit():
+            print(True)
+            break
+    else:
+        print(False)    
+    
+    for i in range(len(s)):
+        if s[i].islower():
+            print(True)
+            break
+    else:
+        print(False)    
+
+    for i in range(len(s)):
+        if s[i].isupper():
+            print(True)
+            break
+    else:
+        print(False)  
+
+
+if __name__ == '__main__':
+    s = input()
+    main(s)    
